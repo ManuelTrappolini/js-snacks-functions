0c3a2f549a4564157abe3d3c6be8eef5151c3ba9
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-10-07 17:04:24 +0200

    add exercise 4

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -4,10 +4,17 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+function helloThere(name){
+   askName = prompt('Inserisci il tuo nome qui')
+   let greeting = `Ciao ${askName}` 
+   console.log(greeting);
+   alert(`Ciao ${askName}`)
+   
 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+helloThere()
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
