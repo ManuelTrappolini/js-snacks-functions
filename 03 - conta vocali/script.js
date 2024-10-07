d824cc87b2e97df5f1929a8c9d53530cb283090e
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-10-07 16:51:32 +0200

    add exercise 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,23 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function searchForVowels(){
+    let vowels = ['a','e','i','o','u']
+    let count = 0
+    for(let i = 0; i < word.length; i++){
+        
+        if(vowels.includes(word[i])){
+            
+            count ++
+        }
+        
+    }
+    console.log(word, count,);
+    
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+searchForVowels()
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
